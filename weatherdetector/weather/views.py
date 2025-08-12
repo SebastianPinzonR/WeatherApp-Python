b15286174fad5254d138b9e67ccfecedee412582
Sebastian <spinzonrojas@gmail.com>
2025-08-11 22:08:35 -0500

    Funciona, pero se le quito la api key, de modo que no cojera los valores de el clima

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         # Handle form submission if needed
         city = request.POST['city']
-    return render(request, 'index.html')  # Render the index template
+        res = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=AQUI SE PONE LA API KEY').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code":str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        data = {}
+    return render(request, 'index.html', {'data':data})  # Render the index template
